e84d5dbeb735823a91ba08116654965ab3eec825
daniel <danielclarke@wearepopgun.com>
2023-04-05 22:50:20 +1000

    tidy cpp

--- cpp/build-native.sh
+++ cpp/build-native.sh
@@ -1,8 +1,8 @@
 export MAKEFLAGS="-j4"
-export CPPFLAGS="-I/$(brew --prefix llvm)/include"
-export LDFLAGS="-L/$(brew --prefix llvm)/lib -Wl,-rpath,/$(brew --prefix llvm)/lib"
-export CXX="/$(brew --prefix llvm)/bin/clang++"
-export CC="/$(brew --prefix llvm)/bin/clang"
+export CPPFLAGS="-I$(brew --prefix llvm)/include"
+export LDFLAGS="-L$(brew --prefix llvm)/lib -Wl,-rpath,$(brew --prefix llvm)/lib"
+export CXX="$(brew --prefix llvm)/bin/clang++"
+export CC="$(brew --prefix llvm)/bin/clang"
 cmake -S . -B build
 cmake --build build
 cp ./assets/index.html ./build
